35d06c5e47c6123e3a655dcf8170b4ecfeef0fcd
Adam Jimerson <vendion@gmail.com>
2014-12-14 04:14:42 +0000

    Authiticate with Google Oauth2 service
    Add authitication with Google's Oauth2 service, and make a GET request
    to Google Calendar API to fetch event list.
    
    Signed-off-by: Adam Jimerson <vendion@gmail.com>

--- events.go
+++ events.go
@@ -1,42 +1,66 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"net/url"
 	"os"
+	"strings"
+	"time"
 
 	"github.com/danryan/hal"
-	"golang.org/x/oauth2"
-	"golang.org/x/oauth2/google"
 )
 
 var eventHandler = hal.Hear(`.events`, func(res *hal.Response) error {
 	events, err := getCalendarEvents()
 	if err != nil {
-		return res.Send("Could not fetch list of events")
+		return res.Send(err.Error())
 	}
 
 	return res.Send(events)
 })
 
-var baseURL = "https://www.googleapis.com/calendar/v3"
+var (
+	baseURL     = "https://www.googleapis.com/calendar/v3/calendars"
+	accessToken AccessToken
+)
+
+type AccessToken struct {
+	Token string
+	Valid time.Time
+}
+
+func (a *AccessToken) getExpireTime(o int) {
+	n := time.Now()
+	d := time.Duration(o) * time.Second
+
+	a.Valid = n.Add(d)
+}
+
+func (a *AccessToken) expiredToken() bool {
+	n := time.Now()
+	if a.Valid.After(n) {
+		return true
+	}
+
+	return false
+}
 
 func getCalendarEvents() (string, error) {
-	oauthToken := os.Getenv("CHADEV-TOKEN")
-	var opts *oauth2.Options
 	var err error
-	if oauthToken == "" {
-		opts, err = getOauth2Token()
+
+	if accessToken.Token == "" ||
+		accessToken.expiredToken() {
+		accessToken, err = getOauth2Token()
 		if err != nil {
 			return "", err
 		}
 	}
 
-	url := opts.AuthCodeURL("test", "offline", "auto")
-	return url, nil
-
-	URL := fmt.Sprintf("%s/calendars/%s/events", baseURL, "4qc3thgj9ocunpfist563utr6g")
+	URL := fmt.Sprintf("%s/4qc3thgj9ocunpfist563utr6g@group.calendar.google.com/events?access_token=%s",
+		baseURL, url.QueryEscape(accessToken.Token))
 	resp, err := http.Get(URL)
 	if err != nil {
 		return "", err
@@ -51,18 +75,40 @@ func getCalendarEvents() (string, error) {
 	return string(body), nil
 }
 
-func getOauth2Token() (*oauth2.Options, error) {
-	clientID := os.Getenv("CHADEV-ID")
-	clientSecret := os.Getenv("CHADEV-SECRET")
-	opts, err := oauth2.New(
-		oauth2.Client(clientID, clientSecret),
-		oauth2.RedirectURL("urn:ietf:wg:oauth:2.0:oob"),
-		oauth2.Scope("https://www.googleapis.com/auth/calendar.readonly"),
-		google.Endpoint(),
-	)
-	if err != nil {
-		return nil, err
+func getOauth2Token() (AccessToken, error) {
+	type Responce struct {
+		Token   string `json:"access_token"`
+		Expires int    `json:"expires_in"`
 	}
 
-	return opts, nil
+	clientID := os.Getenv("CHADEV_ID")
+	clientSecret := os.Getenv("CHADEV_SECRET")
+	refreshToken := os.Getenv("CHADEV_TOKEN")
+
+	var r Responce
+
+	body := fmt.Sprintf("client_id=%s&client_secret=%s&refresh_token=%s&grant_type=refresh_token",
+		clientID, clientSecret, refreshToken)
+	b := strings.NewReader(body)
+
+	resp, err := http.Post("https://accounts.google.com/o/oauth2/token", "application/x-www-form-urlencoded", b)
+	if err != nil {
+		return accessToken, err
+	}
+	defer resp.Body.Close()
+
+	respBody, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return accessToken, err
+	}
+
+	err = json.Unmarshal(respBody, &r)
+	if err != nil {
+		return accessToken, err
+	}
+
+	accessToken.Token = r.Token
+	accessToken.getExpireTime(r.Expires)
+
+	return accessToken, nil
 }
