da7c9a21dfd7b9003b70d166fd240ad630708b60
Andrew Rodgers <acedrew@gmail.com>
2014-12-20 03:43:20 +0000

    corrected formatting

--- fizzbuzz.go
+++ fizzbuzz.go
@@ -1,4 +1,3 @@
-
 // Copyright 2014 Chadev. All rights reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
@@ -16,15 +15,15 @@ var fizzBuzzHandler = hear(`fb ([0-9]+)`, func(res *hal.Response) error {
 	if err != nil {
 		res.Send("What are you even?")
 		return err
-		}
+	}
 	switch {
-        case i%15==0:
-			return res.Send("FizzBuzz")
-        case i%3==0:
-			return res.Send("Fizz")
-        case i%5==0:
-			return res.Send("Buzz")
-        default:
-            return res.Send(res.Match[1])
-        }
+	case i%15 == 0:
+		return res.Send("FizzBuzz")
+	case i%3 == 0:
+		return res.Send("Fizz")
+	case i%5 == 0:
+		return res.Send("Buzz")
+	default:
+		return res.Send(res.Match[1])
+	}
 })
