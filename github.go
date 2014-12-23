9185fab774988ef939127b53ea42a85d03b3dd17
Adam Jimerson <vendion@gmail.com>
2014-12-23 20:51:59 +0000

    Link to an issue for a project
    If the issue command is given both a project name and issue ID, it will
    return the URL for that issue.
    
    Signed-off-by: Adam Jimerson <vendion@gmail.com>

--- github.go
+++ github.go
@@ -25,10 +25,30 @@ var sourceHandler = hear(`source(.*)`, func(res *hal.Response) error {
 })
 
 var issueHandler = hear(`issue(.*)`, func(res *hal.Response) error {
-	URL, err := getIssueURL(strings.TrimSpace(res.Match[1]))
+	args := make([]string, 2)
+	if res.Match[1] != "" {
+		res.Match[1] = strings.TrimSpace(res.Match[1]) // trim leading and tailing whitespace from the match
+		// check if we have a project name and issue nubmer "projectName issueID"
+		if strings.Contains(res.Match[1], " ") {
+			args = strings.Split(res.Match[1], " ")
+		} else {
+			args[0] = res.Match[1]
+			args[1] = ""
+		}
+	}
+
+	URL, err := getIssueURL(args[0])
 	if err != nil {
 		hal.Logger.Error(fmt.Sprintf("unable to get issue URL: %v\n", err))
-		return res.Send(fmt.Sprintf("Fetching issue queue URL for %s failed, possibly misspelled?", res.Match[1]))
+		return res.Send(fmt.Sprintf("Fetching issue queue URL for %s failed, possibly misspelled?", args[0]))
+	}
+
+	if args[1] != "" {
+		URL, err = getIssueIDURL(URL, strings.TrimLeft(args[1], "#"))
+		if err != nil {
+			hal.Logger.Error(fmt.Sprintf("unable to get issue URL: %v\n", err))
+			return res.Send(fmt.Sprintf("Fetching issue URL for issue %s failed", args[1]))
+		}
 	}
 
 	return res.Send(URL)
@@ -59,6 +79,17 @@ func getIssueURL(s string) (string, error) {
 
 	if !validateURL(URL) {
 		return "", errors.New("unable to get GitHub URL: no repo with URL: " + URL)
+	}
+
+	return URL, nil
+}
+
+func getIssueIDURL(u, i string) (string, error) {
+	// build the URL
+	URL := fmt.Sprintf("%s/%s", u, i)
+
+	if !validateURL(URL) {
+		return "", errors.New("unable to get issue URL: no repo or issue with URL: " + URL)
 	}
 
 	return URL, nil
