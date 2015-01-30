d481dbeb7ff1196e3195e80ec192dccf36add412
Adam Jimerson <vendion@gmail.com>
2015-01-30 19:25:55 +0000

    Add command to return music URLs
    Signed-off-by: Adam Jimerson <vendion@gmail.com>

---
+++ music.go
@@ -0,0 +1,22 @@
+// Copyright 2014 Chadev. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package main
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/danryan/hal"
+)
+
+var musicHandler = hear(`give me some music`, "give me some music", "Returns a list of music playlists popular with the community", func(res *hal.Response) error {
+	pl := []string{
+		"http://open.spotify.com/user/juzten/playlist/0yCFUrwFvi4lu19DfkMcuH",
+	}
+
+	list := strings.Join(pl, ",")
+
+	return res.Send(fmt.Sprintf("%s: here try out these awesome tunes %v", res.UserName(), list))
+})
