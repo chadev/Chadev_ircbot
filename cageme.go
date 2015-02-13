6c0b64261ae45d2aca7fff97bd335389ec8cc6d7
Adam Jimerson <vendion@gmail.com>
2015-02-13 03:24:10 +0000

    Update copyright notices
    Signed-off-by: Adam Jimerson <vendion@gmail.com>

--- cageme.go
+++ cageme.go
@@ -1,4 +1,4 @@
-// Copyright 2014 Chadev. All rights reserved.
+// Copyright 2014-2015 Chadev. All rights reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
