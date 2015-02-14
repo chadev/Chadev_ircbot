2940b7fbe1b2cf2dd81fd50963a2d03a471c0d1e
Adam Jimerson <vendion@gmail.com>
2015-02-14 13:42:37 +0000

    Add datetime to dev lunch output
    Signed-off-by: Adam Jimerson <vendion@gmail.com>

--- devlunch.go
+++ devlunch.go
@@ -44,6 +44,7 @@ type MeetupEvents struct {
 	Venue    MeetupVenue `json:"venue"`
 	EventURL string      `json:"event_url"`
 	Name     string      `json:"name"`
+	Time     int64       `json:"time"`
 }
 
 type MeetupVenue struct {
@@ -52,10 +53,39 @@ type MeetupVenue struct {
 
 func (m *Meetup) string(lunchDay bool) string {
 	if !lunchDay {
-		return fmt.Sprintf("The next talk is \"%s\", you can join us at %s.  If you plan to come please make sure you have RSVPed at %s", m.Results[0].Name, m.Results[0].Venue.Name, m.Results[0].EventURL)
+		return fmt.Sprintf("The next talk is \"%s\", you can join us at %s on %s.  If you plan to come please make sure you have RSVPed at %s",
+			m.Results[0].Name,
+			m.Results[0].Venue.Name,
+			m.Results[0].parseDateTime(false),
+			m.Results[0].EventURL)
 	}
 
-	return fmt.Sprintf("The talk today is \"%s\", you can join us at %s.  If you plan to come please make sure you have RSVPed at %s", m.Results[0].Name, m.Results[0].Venue.Name, m.Results[0].EventURL)
+	return fmt.Sprintf("The talk today is \"%s\", you can join us at %s on %s.  If you plan to come please make sure you have RSVPed at %s",
+		m.Results[0].Name,
+		m.Results[0].Venue.Name,
+		m.Results[0].parseDateTime(true),
+		m.Results[0].EventURL)
+}
+
+func (e *MeetupEvents) parseDateTime(today bool) string {
+	// set the timezone, otherwise UTC will be used
+	loc, err := time.LoadLocation("America/New_York")
+	if err != nil {
+		// couldn't set timezone, falling back to UTC
+		loc, _ = time.LoadLocation("UTC")
+	}
+
+	// parse the unix timestamp, and apply our timezone
+	t := time.Unix(0, e.Time*int64(time.Millisecond)).In(loc)
+
+	var fT string
+	if today {
+		fT = t.Format("13:05")
+	} else {
+		fT = t.Format("01/02 13:05")
+	}
+
+	return fT
 }
 
 func getTalkDetails(lunchDay bool) (string, error) {
