82c8e2e798d2afe3724cb1de7e12966e84c9e195
Adam Jimerson <vendion@gmail.com>
2015-03-03 13:49:25 +0000

    Version bump to 2015-03-03
    Signed-off-by: Adam Jimerson <vendion@gmail.com>

--- main.go
+++ main.go
@@ -16,7 +16,7 @@ import (
 )
 
 // VERSION contians the current verison number and revison if need be
-const VERSION = "2015-03-02"
+const VERSION = "2015-03-03"
 
 // handler is an interface for objects to implement in order to respond to messages.
 type handler interface {
