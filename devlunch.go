019115d8820b689b0b0ea329db572c1bcac6d135
Adam Jimerson <vendion@gmail.com>
2015-03-04 23:38:48 +0000

    Move generic meetup funciton/types
    Signed-off-by: Adam Jimerson <vendion@gmail.com>

--- devlunch.go
+++ devlunch.go
@@ -16,29 +16,6 @@ import (
 	"github.com/danryan/hal"
 )
 
-// Meetup contains the ruturn value from the Meetup API.
-type Meetup struct {
-	Results []MeetupEvents `json:"results"`
-}
-
-// MeetupEvents contains details for each event in the return value.
-type MeetupEvents struct {
-	Venue    MeetupVenue `json:"venue"`
-	EventURL string      `json:"event_url"`
-	Name     string      `json:"name"`
-	Time     int64       `json:"time"`
-}
-
-// MeetupVenue contains details about the venue for each event.
-type MeetupVenue struct {
-	Name string `json:"name"`
-}
-
-// DevTalk contains the dev talk live stream details.
-type DevTalk struct {
-	Date, URL string
-}
-
 var lunchHandler = hear(`is today (devlunch|dev lunch) day\b`, "is today devlunch day", "Tells if today is lunch day, and what the talk is", func(res *hal.Response) error {
 	d := time.Now().Weekday().String()
 	if d != "Thursday" {
@@ -153,27 +130,6 @@ func (m *Meetup) string(lunchDay bool) string {
 		m.Results[0].Venue.Name,
 		m.Results[0].parseDateTime(true),
 		m.Results[0].EventURL)
-}
-
-func (e *MeetupEvents) parseDateTime(today bool) string {
-	// set the timezone, otherwise UTC will be used
-	loc, err := time.LoadLocation("America/New_York")
-	if err != nil {
-		// couldn't set timezone, falling back to UTC
-		loc, _ = time.LoadLocation("UTC")
-	}
-
-	// parse the unix timestamp, and apply our timezone
-	t := time.Unix(0, e.Time*int64(time.Millisecond)).In(loc)
-
-	var fT string
-	if today {
-		fT = t.Format("3:04 pm")
-	} else {
-		fT = t.Format("01/02 3:04 pm")
-	}
-
-	return fT
 }
 
 func getTalkDetails(lunchDay bool) (string, error) {
