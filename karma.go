eea27b7b4a678a875816c1509296008ba4b7aa09
Adam Jimerson <vendion@gmail.com>
2015-06-09 08:17:46 -0400

    Use a struct for sturturing karma rating
    Signed-off-by: Adam Jimerson <vendion@gmail.com>

--- karma.go
+++ karma.go
@@ -5,35 +5,44 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
-	"strconv"
 	"strings"
 
 	"github.com/danryan/hal"
 )
 
+type DevKarma struct {
+	Name  string `json:"nick"`
+	Karma int    `json:"karma"`
+}
+
 var karmaHandler = hear(`(.+)(\+\+|\-\-)`, "(nickname)++ or (nickname)--", "Increases or Decreases a persons karma", func(res *hal.Response) error {
 	nick := res.Match[1]
 	sign := res.Match[2]
 
-	var karma int
+	var dev DevKarma
 	key := strings.ToUpper(nick)
 	val, _ := res.Robot.Store.Get("Karma::" + key)
-	num, _ := strconv.ParseInt(string(val), 10, 0)
-	karma = int(num)
+	json.Unmarshal(val, &dev)
+
+	// check to see if we got something back
+	if dev.Name == "" {
+		dev.Name = nick
+	}
 
 	if sign == "++" {
-		karma++
+		dev.Karma++
 	} else if sign == "--" {
-		karma--
+		dev.Karma--
 	} else {
 		hal.Logger.Errorf("invalid sign '%v' given", sign)
 		err := res.Reply("that is not a valid option, please try again with either ++ or --")
 		return err
 	}
 
-	s := strconv.Itoa(karma)
-	err := res.Robot.Store.Set("Karma::"+key, []byte(s))
+	val, _ = json.Marshal(dev)
+	err := res.Robot.Store.Set("Karma::"+key, val)
 	if err != nil {
 		hal.Logger.Errorf("could not update the user's karma: %v", err)
 		err = res.Reply("I was unable to update the users karma, please try again later")
@@ -49,17 +58,23 @@ var karmaStatsHandler = hear(`karma stats(.*)`, "karma stats (username)", "Shows
 	if nick == "" {
 		nick = res.UserName()
 	}
-	key := strings.ToUpper(nick)
 
+	var dev DevKarma
+	key := strings.ToUpper(nick)
 	val, err := res.Robot.Store.Get("Karma::" + key)
 	if err != nil {
 		hal.Logger.Infof("user %s has no karma", nick)
 		err = res.Reply(fmt.Sprintf("User %s currently has no karma", nick))
 		return err
 	}
+	json.Unmarshal(val, &dev)
 
-	karma, _ := strconv.Atoi(string(val))
+	if dev.Name == "" {
+		hal.Logger.Infof("user %s has no karma", nick)
+		err = res.Reply(fmt.Sprintf("User %s currently has no karma", nick))
+		return err
+	}
 
-	err = res.Send(fmt.Sprintf("User %s currently has %d karma", nick, karma))
+	err = res.Send(fmt.Sprintf("User %s currently has %d karma", nick, dev.Karma))
 	return err
 })
